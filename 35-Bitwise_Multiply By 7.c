fd4f3c5c7a55c2017520d429885c61b3b70fec1d
Roushan Gupta <roushan01jan@gmail.com>
2025-02-18 23:56:19 +0530

    Add files via upload

---
+++ 35-Bitwise_Multiply By 7.c
@@ -0,0 +1,35 @@
+/*
+Write a program to multiply a given integer n by 7 using only bitwise operators. You are not allowed to use the multiplication (*) operator directly.
+
+Input Format
+
+A single integer n.
+Constraints
+
+−10^5 ≤ n ≤ 10^5
+Output Format
+
+Print a single integer: the result of n×7.
+Sample Input 0
+
+1
+Sample Output 0
+
+7
+Sample Input 1
+
+3
+Sample Output 1
+
+21
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    scanf("%d",&num);
+    printf("%d",(num<<3)-(num));
+    return 0;
+}
