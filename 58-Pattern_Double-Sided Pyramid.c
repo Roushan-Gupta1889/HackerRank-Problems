9d7e1fdf4b3282ed85dde584d47b88f486056fba
Roushan Gupta <roushan01jan@gmail.com>
2025-02-25 23:45:54 +0530

    Add files via upload

---
+++ 58-Pattern_Double-Sided Pyramid.c
@@ -0,0 +1,62 @@
+/*
+Write a program to print a double-sided pyramid pattern based on an integer input n. Each row of the pyramid should contain left-aligned and right-aligned patterns of asterisks separated by two spaces.
+
+Note: Handle invalid inputs gracefully by displaying an appropriate error message, "Shape Not Possible", if the double-sided pyramid cannot be drawn by the given input value.
+
+Input Format
+
+A single integer n.
+Constraints
+
+-50 ≤ n ≤ 50
+Output Format
+
+Print the double-sided pyramid pattern consisting on n rows.
+Print "Shape Not Possible" if the pattern cannot be drawn.
+Sample Input 0
+
+5
+Sample Output 0
+
+    *  *
+   **  **
+  ***  ***
+ ****  ****
+*****  *****
+Sample Input 1
+
+1
+Sample Output 1
+
+Shape Not Possible
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int rows;
+    scanf("%d",&rows);
+    if (rows>1){
+        for (int i=1;i<=rows;++i){
+            for (int k=2;k<=rows-i+1;++k){
+                printf(" ");
+            }
+            for (int j=1;j<=i;++j){
+                printf("*");
+            }
+           
+               printf("  ");
+           
+            for (int j=1;j<=i;++j){
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+    else{
+        printf ("Shape Not Possible");
+    }
+    
+    return 0;
+}
